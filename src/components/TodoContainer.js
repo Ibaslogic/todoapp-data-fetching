ab80e0effc52893a347a42e20caef920d378466d
ibaslogic <ibaskunle@gmail.com>
2020-03-18 23:50:57 +0100

    first commit

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,73 @@
+import React from "react";
+import TodosList from "./TodosList";
+import Header from "./Header";
+import InputTodo from "./InputTodo";
+
+import axios from "axios";
+
+class TodoContainer extends React.Component {
+  state = {
+    todos: [],
+    show: false
+  };
+
+  handleChange = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+      show: !this.state.show
+    });
+  };
+
+  delTodo = id => {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(reponse =>
+        this.setState({
+          todos: [
+            ...this.state.todos.filter(todo => {
+              return todo.id !== id;
+            })
+          ]
+        })
+      );
+  };
+
+  addTodoItem = title => {
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", {
+        title: title,
+        completed: false
+      })
+      .then(response =>
+        this.setState({
+          todos: [...this.state.todos, response.data]
+        })
+      );
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(response => this.setState({ todos: response.data }));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header headerSpan={this.state.show} />
+        <InputTodo addTodoProps={this.addTodoItem} />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
+        />
+      </div>
+    );
+  }
+}
+export default TodoContainer;
